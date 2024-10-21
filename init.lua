c99914be97bc5b4688799841248fc92c348c8a6d
luca <baccinelliluca@gmail.com>
2024-10-21 23:55:24 +0200

    added lsp-config and small refactors

--- init.lua
+++ init.lua
@@ -20,5 +20,14 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- line numbers (relative)
+vim.wo.relativenumber = true
+
+-- tabs
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
 -- Setup lazy.nvim
 require("lazy").setup("plugins")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,32 @@
 return {
-	'nvim-telescope/telescope.nvim', 
-	tag = '0.1.8',
-	dependencies = { 
-		'nvim-lua/plenary.nvim'
-	},
-	keys = function() 
-		return {
-			{
-				'<C-p>',
-				"<cmd>Telescope find_files<cr>",
-				desc = 'Telescope file finder'
+	{
+		'nvim-telescope/telescope.nvim', 
+		tag = '0.1.8',
+		dependencies = { 
+			'nvim-lua/plenary.nvim'
+		},
+		keys = function() 
+			return {
+				{
+					'<C-p>',
+					"<cmd>Telescope find_files<cr>",
+					desc = 'Telescope file finder'
+				}
 			}
-		}
-	end
+		end
+	},
+	{
+		'nvim-telescope/telescope-ui-select.nvim',
+		config = function()
+			require("telescope").setup {
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown{}
+			    	}
+				}
+			}
+			require("telescope").load_extension("ui-select")
+		end
+
+	}
 }
