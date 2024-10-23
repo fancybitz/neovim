c07c811bebd1f0faae0d7b30cb79505520b422a0
luca <baccinelliluca@gmail.com>
2024-10-23 09:23:43 +0200

    added neotree configs

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -14,5 +14,33 @@ return {
 				desc = "Toggle Neotree"
 			}
 		}
-	end
+	end,
+    config = function() 
+        require('neo-tree').setup({
+            visible = true,
+            close_if_last_window = false,
+            popup_border_style = "rounded",
+            enable_git_status = true,
+            enable_diagnostics = true,
+            default_component_cs = {
+                container = {
+                    enable_character_fade = true
+                },
+                git_status = {
+                    symbols = {
+                        added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                        modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                        deleted   = "✖",-- this can only be used in the git_status source
+                        renamed   = "󰁕",-- this can only be used in the git_status source
+                        -- Status type
+                        untracked = "",
+                        ignored   = "",
+                        unstaged  = "󰄱",
+                        staged    = "",
+                        conflict  = ""
+                    }
+                }
+            },
+        })
+    end
 }
